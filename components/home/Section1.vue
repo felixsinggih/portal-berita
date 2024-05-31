1b02c9092473a91381fd8b5b6691f46d52a76871
Felix Singgih <felixsinggih@gmail.com>
2024-05-31 19:57:21 +0700

    disable popular article

--- components/home/Section1.vue
+++ components/home/Section1.vue
@@ -5,13 +5,13 @@
 <template>
   <div class="pt-4 flex flex-col gap-6 lg:flex-row">
     <!-- Headline -->
-    <div class="w-full lg:w-3/5">
+    <div class="w-full">
       <HomeFeaturedArticle />
     </div>
 
     <!-- Popular Article -->
-    <div class="w-full lg:w-2/5">
+    <!-- <div class="w-full lg:w-2/5">
       <LazyHomePopularArticle />
-    </div>
+    </div> -->
   </div>
 </template>
